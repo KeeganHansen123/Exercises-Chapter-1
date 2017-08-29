d7a4c79479b9d2870e2651870107c7575e457a61
KeeganHansen123 <kh311508@students.davenport.k12.ia.us>
2017-08-29 09:26:26 -0500

    Update to Git first time

---
+++ Chapter1/src/excercises/MovieQuoteInfo.java
@@ -0,0 +1,14 @@
+package excercises;
+
+import javax.swing.JOptionPane;
+
+public class MovieQuoteInfo {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null,"You either die a Hero or live long enough to see yourself become the Villain.");
+		JOptionPane.showMessageDialog(null, "Movie: The Dark Knight, Character: Harvey Dent, Year of Release: 2008");
+	}
+
+}
---
+++ Chapter1/src/excercises/TableandChairs.java
@@ -0,0 +1,23 @@
+package excercises;
+
+public class TableandChairs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("C                                                                                      C");	
+		System.out.println("C                                                                                      C");	
+		System.out.println("C                                                                                      C");	
+		System.out.println("C                                                                                      C");	
+		System.out.println("C                                                                                      C");	
+		System.out.println("C                                                                                      C");	
+		System.out.println("C												                                       C");	
+		System.out.println("CCCCCCCCCCCCC                       CCCCCCCCCCCCCC                         CCCCCCCCCCCCC");     
+		System.out.println("C           C	                    C            C                         C           C");
+		System.out.println("C           C                       C            C                         C           C");
+		System.out.println("C           C                       C            C                         C           C");
+		System.out.println("C           C                       C            C                         C           C");
+		System.out.println("C           C                       C            C                         C           C");
+	
+	}
+	
+}
