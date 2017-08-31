241f5dd2eb9ebd57e70f14f49f4df27550b0d2e5
KeeganHansen123 <kh311508@students.davenport.k12.ia.us>
2017-08-31 09:06:58 -0500

    update to current folder

--- Chapter1/src/excercises/FavoriteSong.java
+++ Chapter1/src/excercises/FavoriteSong.java
@@ -1,10 +1,16 @@
 package excercises;
 
+import javax.swing.JOptionPane;
+
 public class FavoriteSong {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
-
+		JOptionPane.showMessageDialog(null, "I'm gonna fight'em all");
+		JOptionPane.showMessageDialog(null, "A seven nation army couldn't hold me back");
+		JOptionPane.showMessageDialog(null, "They're gonna rip it off");
+		JOptionPane.showMessageDialog(null, "Taking their time right behind my back");
 	}
 
 }
