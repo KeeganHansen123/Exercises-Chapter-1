c8c2e81a50f43ae08deddb41c78cce958e020de0
KeeganHansen123 <kh311508@students.davenport.k12.ia.us>
2017-08-31 09:05:24 -0500

    update to current folder

--- Chapter1/src/excercises/TableandChairs.java
+++ Chapter1/src/excercises/TableandChairs.java
@@ -3,20 +3,17 @@ package excercises;
 public class TableandChairs {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("C                                                                                      C");	
-		System.out.println("C                                                                                      C");	
-		System.out.println("C                                                                                      C");	
-		System.out.println("C                                                                                      C");	
-		System.out.println("C                                                                                      C");	
-		System.out.println("C                                                                                      C");	
-		System.out.println("C												                                       C");	
-		System.out.println("CCCCCCCCCCCCC                       CCCCCCCCCCCCCC                         CCCCCCCCCCCCC");     
-		System.out.println("C           C	                    C            C                         C           C");
-		System.out.println("C           C                       C            C                         C           C");
-		System.out.println("C           C                       C            C                         C           C");
-		System.out.println("C           C                       C            C                         C           C");
-		System.out.println("C           C                       C            C                         C           C");
+		// TODO Auto-generated method stub	
+		System.out.println("C                                                 C");	
+		System.out.println("C                                                 C");	
+		System.out.println("C                                                 C");	
+		System.out.println("C                                                 C");	
+		System.out.println("C												  C");	
+		System.out.println("CCCCCCCCC         CCCCCCCCCCCCCC          CCCCCCCCC");     
+		System.out.println("C       C	      C            C          C       C");
+		System.out.println("C       C         C            C          C       C");
+		System.out.println("C       C         C            C          C       C");
+		
 	
 	}
 	
